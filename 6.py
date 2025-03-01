3093219aeb59e05fe7d0830f54f897b919913c96
EgorKaufman <138799274+EgorKaufman@users.noreply.github.com>
2025-03-01 22:10:57 +0300

    Add files via upload

---
+++ 6.py
@@ -0,0 +1,7 @@
+class Emoloyee:
+    def show(self, name, salary):
+        return name + ' - ' + salary
+  
+user = Emoloyee()
+user.show('Doynikov', '300 Dollars ')
+print(user.show('Doynikov', '300 Dollars '))
